7a9cbce7bdf9db5c696625f99110137c549c0444
Ioannis Michos <giannismichos004@gmail.com>
2025-04-15 16:04:13 +0300

    Προσθήκη exceptions και Interface IStudentService για τον Μαθητή

---
+++ src/main/java/gr/aueb/cf/schoolapp/service/IStudentService.java
@@ -0,0 +1,21 @@
+package gr.aueb.cf.schoolapp.service;
+
+import gr.aueb.cf.schoolapp.dto.*;
+import gr.aueb.cf.schoolapp.exceptions.StudentAlreadyExistsException;
+import gr.aueb.cf.schoolapp.exceptions.StudentDAOException;
+import gr.aueb.cf.schoolapp.exceptions.StudentNotFoundException;
+
+
+import java.util.List;
+
+public interface IStudentService {
+    StudentReadOnlyDTO insertStudent(StudentInsertDTO dto)
+            throws StudentDAOException, StudentAlreadyExistsException;
+    StudentReadOnlyDTO updateStudent(StudentUpdateDTO dto)
+            throws StudentDAOException, StudentAlreadyExistsException, StudentNotFoundException;
+    void deleteStudent(Integer id)  throws StudentDAOException, StudentNotFoundException;
+    StudentReadOnlyDTO getStudentById(Integer id) throws StudentDAOException, StudentNotFoundException;
+    List<StudentReadOnlyDTO> getAllStudents() throws StudentDAOException;
+    List<StudentReadOnlyDTO> getStudentByLastname(String lastname) throws StudentDAOException;
+    List<StudentReadOnlyDTO> getFilteredStudent(FiltersDTO filters) throws StudentDAOException;
+}
